d2aa6b281c05067523459984b44c51f86b526199
Nenad <nenad.stojanovic87@gmail.com>
2021-08-05 20:16:04 +0200

    Section Staples

--- js/app.js
+++ js/app.js
@@ -1,35 +1,19 @@
-$(document).ready(function(){
+$(function(){
 $('.staples__slider').slick({
-  dots: false,
-  infinite: true,
+  infinite: false,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
-  arrows: true,
+  nextArrow: '.staples__arrow-next',
+  prevArrow: '.staples__arrow-prev',
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
       breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
       }
-    }
+    },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
